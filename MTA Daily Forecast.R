f8867c70bde7a9e74007235cb7decd41b70d2ef9
Joseph de Rege <jderege@yahoo.com>
2025-05-09 11:32:47 -0400

    Update MTA Daily Forecast.R
    Add plotting information for the different forecasts and tried other forecasting methods

--- MTA Daily Forecast.R
+++ MTA Daily Forecast.R
@@ -15,12 +15,48 @@ MTADailyts |> filter(Mode == "Subway") |> filter(year(Date) > 2023) -> CRZ_data_
 
 CRZ_data_subway |> gg_season()
 CRZ_data_entries |> autoplot()
+CRZ_data_entries |> gg_season(period = "1w")
+CRZ_data_subway |> gg_season(period = "1w")
+
+subwayDailyts |>
+  autoplot() + 
+  labs(x = "Date", y = "Ridership")
 
 subwayDailyts |> 
   model(
-    STL(Count ~ trend(window = 100) + season(window = 13),
+    STL(Count ~ trend(window = 99) + season(window = 13),
+        robust = FALSE)) |> components() -> subwayDailytsSTLModel
+
+ggplot(subwayDailytsSTLModel, aes(x = Date, y = Count)) +
+  geom_line(aes(y = Count), color = "grey") + 
+  geom_line(aes(y = trend), color = "red") +
+  labs(x = "Date",y = "Ridership")
+
+subwayDailyts |> 
+  model(
+    STL(Count ~ trend(window = 99) + season(window = 13),
         robust = FALSE)) |> components() |>
   autoplot()
+
+subwayDailyts |> 
+  model(
+    STL(Count ~ trend(window = 99) + season(window = 13),
+        robust = FALSE)) |> components() |>
+  filter_index("2024" ~ .) |>
+  autoplot(season_week)
+
+subwayDailyts |> 
+  model(
+    STL(Count ~ trend(window = 99) + season(window = 13),
+        robust = FALSE)) |> components() |>
+  #filter_index("2024" ~ .) |>
+  autoplot(season_year)
+
+subwayDailyts |>
+  gg_season()
+
+subwayDailyts |>
+  gg_season(period = "1week")
 
 subway_fit <- subwayDailyts |> 
   model(trend_model = TSLM(Count ~ trend()))
@@ -58,3 +94,37 @@ subway_fc |> autoplot(filter_index(subway_train, "2024" ~ .), level = NULL) +
 augment(subway_fit) |>
   autoplot(.innov)
 subway_fit |> augment() |> features(.innov, ljung_box, lag = 365)
+
+subway_fit |> forecast(h = 10) |> autoplot(filter_index(subwayDailyts, "2024-03-01" ~ .))
+
+subway_sim <- subway_fit |> generate (h = 60, times = 2, bootstrap = TRUE)
+
+subwayDailyts |> 
+  filter_index("2024-03-01" ~ .) |>
+  ggplot(aes(x = Date)) +
+  geom_line(aes(y = Count)) + 
+  geom_line(aes(y = .sim, color = as.factor(.rep)), data = subway_sim)
+
+subway_fc |> autoplot(subwayDailyts) + ylim(0,1e+07) 
+
+subway_dcmp <- subwayDailyts |>
+  model(STL(Count ~ trend(window = 100), robust = TRUE)) |>
+  components() |>
+  select(-.model)
+
+subway_dcmp |>
+  model(SNAIVE(season_adjust)) |>
+  forecast() |>
+  autoplot(filter_index(subway_dcmp,"2024" ~ .))
+
+subway_fit_dcmp <- subwayDailyts |>
+  model(stlf = decomposition_model(
+    STL(Count ~ trend(window = 100), robust = TRUE),
+    NAIVE(season_adjust)
+  ))
+
+subway_fit_dcmp |>
+  forecast() |>
+  autoplot(filter_index(subwayDailyts, "2024" ~ .))
+
+subway_fit_dcmp |> gg_tsresiduals()
